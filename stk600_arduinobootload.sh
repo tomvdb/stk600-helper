77a3ea0e73a39308f465fc262c5dccd29c63dd78
Tom <tvdbon@gmail.com>
2016-10-23 13:29:43 +0200

    fixed arduino bootloader and included hex

--- stk600_arduinobootload.sh
+++ stk600_arduinobootload.sh
@@ -5,5 +5,6 @@
 	echo $0
 	echo "*********************************************************************";
 
-avrdude -c stk600 -P usb -p m328p -v -e -U flash:w:../Arduino/hardware/arduino/bootloaders/optiboot/optiboot_atmega328.hex -U lock:w:0x0F:m
+avrdude -c stk600 -P usb -p m328p -v -e -U efuse:w:0x05:m -U hfuse:w:0xD6:m -U lfuse:w:0xFF:m
+avrdude -c stk600 -P usb -p m328p -v -e -U flash:w:optiboot_atmega328.hex -U lock:w:0x0F:m
 
